387d17a24e9b1e1eae9699499ab375edc1a77be0
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-09-05 10:59:44 -0300

    post messages done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/participants', async function (req,res) {
     try {
 		await mongoClient.connect();
         const user = req.body;
-        let haveAlready = false;
+        let haveAlready = true;
         const userSchema = joi.object({
             name: joi.string().required()
         });
@@ -34,12 +34,12 @@ app.post('/participants', async function (req,res) {
             name: user.name
         }).then(user => {
             if(!user){
-                haveAlready = true;
+                haveAlready = false;
             }else{
                 return res.status(409).send('Esse nome já está sendo utilizado, digite outro!'); 
             }
         });
-        if(haveAlready === true){
+        if(haveAlready === false){
             db.collection("users").insertOne({
                 name: user.name,
                 lastStatus: Date.now()
@@ -72,6 +72,45 @@ app.get('/participants', async function (req,res) {
      }
 });
 
+app.post('/messages', async function (req,res) {
+    try {
+		await mongoClient.connect();
+        const message = req.body;
+        const user = req.headers.user;
+        let haveAlready = false;
+        const userSchema = joi.object({
+            to: joi.string().required(),
+            text: joi.string().required(),
+            type: joi.valid('message').valid('private_message').optional()
+        });
+        const validation = userSchema.validate(message, { abortEarly: true });
+        if (validation.error) {
+            return res.status(422).send('Digite a mensagem corretamente!');
+        }
+        await db.collection("users").findOne({
+            name: user
+        }).then(user => {
+            if(!user){
+                return res.status(422).send('Remetente não está na sala!');
+            }else{
+                haveAlready = true;
+            }
+        });
+        if(haveAlready === true){
+            db.collection("messages").insertOne({
+                from: user, 
+                to: message.to,
+                text: message.text, 
+                type: message.type, 
+                time: dayjs().format('HH:mm:ss')
+            });
+        }
+        return res.status(201).send(); 
+	 } catch (error) {
+	    res.status(500).send('Não foi possível conectar ao servidor!');
+		mongoClient.close();
+	 }
+});
 
 
 app.listen(5000);
